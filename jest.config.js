dd7dd8ef8a16b9c3938ce7d47e0f72a76811bc70
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-03-13 23:33:19 +0700

    Configure jest
    jest.ts setup test

---
+++ jest.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/mocks/**",
+  ],
+  coveragePathIgnorePatterns: [],
+  setupFilesAfterEnv: ["./config/jest/setupTests.js"],
+  testEnvironment: "jsdom",
+  modulePaths: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
+    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
+    "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)":
+      "<rootDir>/config/jest/fileTransform.js",
+  },
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
+  modulePaths: ["<rootDir>/src"],
+  moduleNameMapper: {
+    "^react-native$": "react-native-web",
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+  },
+  moduleFileExtensions: [
+    // Place tsx and ts to beginning as suggestion from Jest team
+    // https://jestjs.io/docs/configuration#modulefileextensions-arraystring
+    "tsx",
+    "ts",
+    "web.js",
+    "js",
+    "web.ts",
+    "web.tsx",
+    "json",
+    "web.jsx",
+    "jsx",
+    "node",
+  ],
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+  resetMocks: true,
+};
