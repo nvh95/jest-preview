438758ed4d749f2a04c6627b0be58880facc3632
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-03-13 23:42:13 +0700

    Use variable environment in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
+        <p>{`My favorite fruit is ${
+          process.env.VITE_APP_FAVORITE_FRUIT || "banana"
+        }`}</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
@@ -27,7 +29,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +41,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,9 @@
-import {render} from '@testing-library/react'
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-describe('App', () => {
-  it('should work as expected', () => {
-    render(<App />)
+describe("App", () => {
+  it("should work as expected", () => {
+    render(<App />);
+    expect(screen.getByText("My favorite fruit is banana")).toBeInTheDocument();
   });
-})
+});
