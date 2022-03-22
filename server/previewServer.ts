024cabd5eb4dc77e5316deb2e16f230367d8e2c4
Truong Nguyen <ntt261298@gmail.com>
2022-03-22 23:59:55 +0700

    feat: Inject css files to html

--- server/previewServer.ts
+++ server/previewServer.ts
@@ -7,7 +7,8 @@ const express = require("express");
 const app = express();
 const port = 3006;
 
-const { readFileSync } = require("fs");
+const path = require("path");
+const { readFileSync, readdirSync } = require("fs");
 
 app.get("/", (req, res) => {
   const html = readFileSync(
@@ -15,15 +16,17 @@ app.get("/", (req, res) => {
     "utf8"
   );
   // console.log(html);
-  // TODO1: HIGH PRIORITY: Hard code App.css. We need to find all css file and inject them to html
   // TODO2: How do we preserve the order of importing css file?
   // For now I think it's not very important, but this is the room for improvement in next versions
   // TODO3: not support styled-components. Might refer to jest-styled-components
   // Target for version > 0.0.1
-  const css = readFileSync(
-    "./node_modules/.cache/jest-preview-dom/App.css",
-    "utf8"
-  );
+  let css = '';
+  const allFiles = readdirSync("./node_modules/.cache/jest-preview-dom");
+  allFiles.forEach((file) => {
+    if (file.endsWith(".css")) {
+      css += readFileSync(`./node_modules/.cache/jest-preview-dom/${path.basename(file)}`, "utf8");
+    }
+  })
   // console.log(css);
   const content = `
   <style>${css}</style>
