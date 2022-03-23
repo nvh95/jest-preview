ae8bec67fa26e7f247565173d0ccafa8222a99cf
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-03-23 23:33:59 +0700

    chore: make 404 page looks nicer

--- demo/__tests__/App.test.tsx
+++ demo/__tests__/App.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 
-import { prettyDOM } from '@testing-library/dom';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import preview from '../../dist/index.es';
@@ -8,12 +7,6 @@ import preview from '../../dist/index.es';
 describe('App', () => {
   it('should work as expected', () => {
     const { container } = render(<App />);
-    console.log(
-      prettyDOM(container, 100, {
-        escapeString: false,
-        highlight: false,
-      }),
-    );
 
     userEvent.click(screen.getByTestId('increase'));
     userEvent.click(screen.getByTestId('increase'));
