e3b5f88c8816c67e738441ce0a208331bc636fb6
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-03-23 22:33:12 +0700

    feat: add preview function

--- demo/__tests__/App.test.tsx
+++ demo/__tests__/App.test.tsx
@@ -1,39 +1,31 @@
-import { prettyDOM, render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import App from "../App";
-import * as fs from "fs";
+import { render, screen } from '@testing-library/react';
 
-describe("App", () => {
-  it("should work as expected", () => {
+import { prettyDOM } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import preview from '../../dist/index.es';
+
+describe('App', () => {
+  it('should work as expected', () => {
     const { container } = render(<App />);
     console.log(
       prettyDOM(container, 100, {
         escapeString: false,
         highlight: false,
-      })
+      }),
     );
 
-    userEvent.click(screen.getByTestId("increase"));
-    userEvent.click(screen.getByTestId("increase"));
-    userEvent.click(screen.getByTestId("increase"));
-    userEvent.click(screen.getByTestId("increase"));
-    userEvent.click(screen.getByTestId("increase"));
-    userEvent.click(screen.getByTestId("increase"));
-    if (!fs.existsSync("./node_modules/.cache/jest-preview-dom")) {
-      fs.mkdirSync("./node_modules/.cache/jest-preview-dom", {
-        recursive: true,
-      });
-    }
-    fs.writeFileSync(
-      "./node_modules/.cache/jest-preview-dom/index.html",
-      prettyDOM(container, 50000, {
-        highlight: false,
-      }),
-      {
-        encoding: "utf-8",
-        flag: "w",
-      }
-    );
-    expect(screen.getByTestId("count")).toContainHTML("6");
+    userEvent.click(screen.getByTestId('increase'));
+    userEvent.click(screen.getByTestId('increase'));
+    userEvent.click(screen.getByTestId('increase'));
+    userEvent.click(screen.getByTestId('increase'));
+    userEvent.click(screen.getByTestId('increase'));
+    userEvent.click(screen.getByTestId('increase'));
+
+    // Open http://localhost:3336 to see preview
+    // Require to run `jest-preview` before
+    preview(container);
+
+    expect(screen.getByTestId('count')).toContainHTML('6');
   });
 });
