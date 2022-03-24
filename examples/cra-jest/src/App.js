61d7ae054810c3b0dec91a47f6fdb12512575811
Truong Nguyen <ntt261298@gmail.com>
2022-03-24 09:35:31 +0700

    feat: Init CRA example

---
+++ examples/cra-jest/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import logo2 from './assets/images/logo.svg';
+
+import './App.css';
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="logo2" alt="logo2" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
