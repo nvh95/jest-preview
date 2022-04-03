89c508fde8f2f77f75ffabd89500087daf89a4eb
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-04-03 15:58:35 +0700

    feat: Restructure code support styled-components

--- src/preview.ts
+++ src/preview.ts
@@ -7,14 +7,20 @@ export function preview(element: Element): void {
     });
   }
 
-  fs.writeFileSync(
-    './node_modules/.cache/jest-preview-dom/cssinjs.css',
-    getStyle(),
-    {
-      encoding: 'utf-8',
-      flag: 'w',
-    },
-  );
+  try {
+    import('./styled-components').then(({ getStyle }) => {
+      fs.writeFileSync(
+        './node_modules/.cache/jest-preview-dom/styled-components.css',
+        getStyle(),
+        {
+          encoding: 'utf-8',
+          flag: 'w',
+        },
+      );
+    });
+  } catch (error) {
+    // `styled-components` is not in used');
+  }
 
   fs.writeFileSync(
     './node_modules/.cache/jest-preview-dom/index.html',
@@ -25,32 +31,3 @@ export function preview(element: Element): void {
     },
   );
 }
-
-// Reference: https://github.dev/styled-components/jest-styled-components/blob/4b07fde2607516db407a6215d17dfb461b90b491/src/utils.js#L38
-// TODO: Just a Proof of Concept. To refactor
-const { ServerStyleSheet, __PRIVATE__ } = require('styled-components');
-
-if (!__PRIVATE__) {
-  throw new Error('Could neither find styled-components secret internals');
-}
-
-const { mainSheet, masterSheet } = __PRIVATE__;
-
-const sheet = mainSheet || masterSheet;
-const isServer = () => typeof document === 'undefined';
-
-const getHTML = () =>
-  isServer() ? new ServerStyleSheet().getStyleTags() : sheet.toString();
-
-const extract = (regex) => {
-  let style = '';
-  let matches;
-
-  while ((matches = regex.exec(getHTML())) !== null) {
-    style += `${matches[1]} `;
-  }
-
-  return style.trim();
-};
-
-export const getStyle = () => extract(/^(?!data-styled\.g\d+.*?\n)(.*)?\n/gm);
