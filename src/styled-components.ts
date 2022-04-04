f689edecf65aceb568149b0f98a4f4f9a2543f9e
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-04-04 23:57:42 +0700

    fix: fix warning if user's project does not use styled-components

--- src/styled-components.ts
+++ src/styled-components.ts
@@ -1,29 +1,36 @@
 // Credit to styled-components
 // Reference: https://github.com/styled-components/jest-styled-components/blob/4b07fde2607516db407a6215d17dfb461b90b491/src/utils.js#L38
 
-const { ServerStyleSheet, __PRIVATE__ } = require('styled-components');
-
-if (!__PRIVATE__) {
-  throw new Error('Could neither find styled-components secret internals');
-}
-
-const { mainSheet, masterSheet } = __PRIVATE__;
-
-const sheet = mainSheet || masterSheet;
-const isServer = () => typeof document === 'undefined';
-
-const getHTML = () =>
-  isServer() ? new ServerStyleSheet().getStyleTags() : sheet.toString();
-
-const extract = (regex: RegExp) => {
-  let style = '';
-  let matches;
-
-  while ((matches = regex.exec(getHTML())) !== null) {
-    style += `${matches[1]} `;
+let getStyleFunc = undefined as undefined | (() => string);
+try {
+  const { ServerStyleSheet, __PRIVATE__ } = require('styled-components');
+  if (!__PRIVATE__) {
+    throw new Error('Could neither find styled-components secret internals');
   }
 
-  return style.trim();
-};
+  const { mainSheet, masterSheet } = __PRIVATE__;
 
-export const getStyle = () => extract(/^(?!data-styled\.g\d+.*?\n)(.*)?\n/gm);
+  const sheet = mainSheet || masterSheet;
+  const isServer = () => typeof document === 'undefined';
+
+  const getHTML = () =>
+    isServer() ? new ServerStyleSheet().getStyleTags() : sheet.toString();
+
+  const extract = (regex: RegExp) => {
+    let style = '';
+    let matches;
+
+    while ((matches = regex.exec(getHTML())) !== null) {
+      style += `${matches[1]} `;
+    }
+
+    return style.trim();
+  };
+
+  getStyleFunc = () => extract(/^(?!data-styled\.g\d+.*?\n)(.*)?\n/gm);
+} catch (error) {
+  // `styled-components` is not in used
+}
+
+export const getStyle = getStyleFunc;
+export {};
