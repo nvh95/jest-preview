d9a6231a325f274cd60ae4ead19a7e21924f57c5
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-04-09 11:28:00 +0700

    chore: remove unused test code

--- examples/vue-testing-library/src/App.spec.js
+++ examples/vue-testing-library/src/App.spec.js
@@ -3,18 +3,6 @@ import App from './App.vue';
 import preview from 'jest-preview';
 
 test('increments value on click', async () => {
-  // The render method returns a collection of utilities to query your component.
-  const { getByText } = render(App);
+  render(App);
   preview.debug();
-  // // getByText returns the first matching node for the provided text, and
-  // // throws an error if no elements match or if more than one match is found.
-  // getByText('Times clicked: 0')
-
-  // const button = getByText('increment')
-
-  // // Dispatch a native click event to our button element.
-  // await fireEvent.click(button)
-  // await fireEvent.click(button)
-
-  // getByText('Times clicked: 2')
 });
