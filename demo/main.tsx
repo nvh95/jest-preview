fcb4cc668f6f8b5a26fe0f373f447c49ff1d6b19
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-04-09 08:34:38 +0700

    chore: add global.css to real App in the demo

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './global.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
