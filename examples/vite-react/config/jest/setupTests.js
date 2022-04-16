e52d4a147343f89a4cb11724bd6ac46cf714d6d2
Truong Nguyen <ntt261298@gmail.com>
2022-04-16 23:40:31 +0700

    refactor: Remove jest preview configure in setupTests

--- examples/vite-react/config/jest/setupTests.js
+++ examples/vite-react/config/jest/setupTests.js
@@ -1,9 +1,4 @@
 import '@testing-library/jest-dom/extend-expect';
-import { jestPreviewConfigure } from 'jest-preview';
-
-jestPreviewConfigure({
-  externalCss: ['src/index.css', 'src/global-style.scss'],
-});
 
 window.matchMedia = (query) => ({
   matches: false,
