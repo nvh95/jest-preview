aa9b29b4285a1571f2dec44993a4ce359ff54287
Nguyễn Thanh Sơn <thanhsonng.211@gmail.com>
2022-04-20 15:59:13 +0700

    chore: Remove unnecesary comment in Nextjs example

--- examples/nextjs/jest.config.js
+++ examples/nextjs/jest.config.js
@@ -8,8 +8,6 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
