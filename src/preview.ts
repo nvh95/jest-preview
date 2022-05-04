18be4c5031ccdc8c3e54dff5cc98b8666f815435
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-05-04 22:51:21 +0700

    fix: moving style to head, instead of body

--- src/preview.ts
+++ src/preview.ts
@@ -10,28 +10,13 @@ export function debug(element: Element = document.body): void {
     });
   }
 
-  // If user use styled-components
-  // TODO: We actually do not need to write the file, since it's already in `document.head`.
-  // Just try to inject document.head, beside document.body and we can remove following code.
-  if (getStyle) {
-    // TODO: We can send this data via websocket instead of writing to disk
-    fs.writeFileSync(
-      path.join(CACHE_FOLDER, 'jp-styled-components.css'),
-      getStyle(),
-      {
-        encoding: 'utf-8',
-        flag: 'w',
-      },
-    );
-  }
-
-  // TODO: To write header to header.html
-  // To convert body to body.html
-  // chokidar needs to watch both head and body
   // CSS Modules and sass should append css to head, instead of body
 
-  fs.writeFileSync(path.join(CACHE_FOLDER, 'index.html'), element.outerHTML, {
-    encoding: 'utf-8',
-    flag: 'w',
-  });
+  const headChildrenOnly = [...document.head.children].reduce(
+    (prev, current) => prev + current.outerHTML,
+    '',
+  );
+  fs.writeFileSync(path.join(CACHE_FOLDER, 'head.html'), headChildrenOnly);
+  // Always save head.html to disk before body.html, so we don't need to watch head.html
+  fs.writeFileSync(path.join(CACHE_FOLDER, 'body.html'), element.outerHTML);
 }
