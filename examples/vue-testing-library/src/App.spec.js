d2b2144dc2a85195986038d9bdb68753006edfb8
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-05-26 18:00:54 +0700

    chore: Use named export and remove pckage-lock for vue example

--- examples/vue-testing-library/src/App.spec.js
+++ examples/vue-testing-library/src/App.spec.js
@@ -1,8 +1,8 @@
 import { render, fireEvent } from '@testing-library/vue';
 import App from './App.vue';
-import preview from 'jest-preview';
+import { debug } from 'jest-preview';
 
 test('increments value on click', async () => {
   render(App);
-  preview.debug();
+  debug();
 });
