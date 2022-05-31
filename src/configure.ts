067e97386eb4913fa741b58be01ff1b69e10a02e
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-05-31 15:32:22 +0700

    chore: add implementation note for deprecate externalCss option

--- src/configure.ts
+++ src/configure.ts
@@ -51,6 +51,10 @@ export async function jestPreviewConfigure(
   }
 
   externalCss?.forEach((cssFile) => {
+    // TODO: To add a yellow warning here
+    // `chalk` is installed in https://github.com/nvh95/jest-preview/pull/129, so we can merge that PR first
+    // Link to migration guide (haven't written yet)
+    // https://github.com/nvh95/jest-preview/issues/124
     // Avoid name collision
     // Example: src/common/styles.css => cache-src___common___styles.css
     const delimiter = '___';
