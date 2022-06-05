6ef680ae67707b9d660aaffc102b0976acae9537
Huynh Duc Duy <duy.huynh@linecorp.com>
2022-06-06 06:56:04 +0700

    Fix chalk version, prettierify commit

--- cli/index.js
+++ cli/index.js
@@ -22,14 +22,16 @@ const notifier = updateNotifier({
   pkg: require('../package.json'),
   updateCheckInterval: 0, // How often to check for updates
   shouldNotifyInNpmScript: true, // Allows notification to be shown when running as an npm script
-  distTag: 'latest' // Can be use to notify user about pre-relase version
-})
+  distTag: 'latest', // Can be use to notify user about pre-relase version
+});
 
 notifier.notify({
   defer: true, // Try not to annoy user by showing the notification after the process has exited
-  message:[
-      `${chalk.blue('{packageName}')} has an update available: ${chalk.gray('{currentVersion}')} → ${chalk.green('{latestVersion}')}`,
-      `Please run ${chalk.cyan('`{updateCommand}`')} to update.`,
-  ].join('\n')
+  message: [
+    `${chalk.blue('{packageName}')} has an update available: ${chalk.gray(
+      '{currentVersion}',
+    )} → ${chalk.green('{latestVersion}')}`,
+    `Please run ${chalk.cyan('`{updateCommand}`')} to update.`,
+  ].join('\n'),
 });
 // END checks for available update and notify user
