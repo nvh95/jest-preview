88037eda20cc1584beaad856608d58c054395cbd
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-06-20 01:20:14 +0700

    feat: Speed up building types by caching .d.ts files

---
+++ scripts/clearBuildFolder.js
@@ -0,0 +1,20 @@
+// @ts-check
+const path = require('path');
+const fs = require('fs');
+
+const DISTRIBUTION_DIRECTORY = path.resolve('./dist');
+
+if (fs.existsSync(DISTRIBUTION_DIRECTORY)) {
+  const files = fs.readdirSync(DISTRIBUTION_DIRECTORY);
+  files.forEach((file) => {
+    if (file === 'tsconfig.types.tsbuildinfo' || file.endsWith('.d.ts')) {
+      // Cache types files
+      console.log(`Keeping file: ${file}`);
+    } else {
+      console.log(`Removing file: ${file}`);
+      fs.unlinkSync(path.join(DISTRIBUTION_DIRECTORY, file));
+    }
+  });
+} else {
+  console.log('No "dist" folder yet. Do nothing.');
+}
