48d4a4411c91eabfe165230960dd92041c2759de
huyenuet <thannhuyenk59uet@gmail.com>
2022-06-24 16:16:44 +0700

    fix: move cypress package to inside docs-e2e-testing folder

---
+++ docs-e2e-testing/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    supportFile: "support/e2e.{js,jsx,ts,tsx}",
+    specPattern: "e2e/**/*.cy.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
