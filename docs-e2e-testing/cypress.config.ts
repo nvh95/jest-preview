ecae44af07b5d2642b0196eb7c1320971352ee44
huyenuet <thannhuyenk59uet@gmail.com>
2022-06-25 22:23:00 +0700

    chore: disable chromeWebSecurity
    This change is for testing an url opened in a new tab

--- docs-e2e-testing/cypress.config.ts
+++ docs-e2e-testing/cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     supportFile: "support/e2e.{js,jsx,ts,tsx}",
     specPattern: "e2e/**/*.cy.{js,jsx,ts,tsx}",
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
