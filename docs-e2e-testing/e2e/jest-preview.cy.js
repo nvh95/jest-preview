171c8957a2708122fa841e67fdd5dc000a2112cc
huyenuet <thannhuyenk59uet@gmail.com>
2022-06-25 22:23:34 +0700

    feat: write more tests for Docs, API, Blog, Demo pages

--- docs-e2e-testing/e2e/jest-preview.cy.js
+++ docs-e2e-testing/e2e/jest-preview.cy.js
@@ -1,7 +1,41 @@
 describe('Tests for Jest Preview', () => {
-  it('Check Jest Preview website is alive', () => {
+  beforeEach( function() {
     cy.visit('https://www.jest-preview.com/')
     cy.contains('h1', 'Jest Preview')
-    cy.contains('Debug your Jest tests. Effortlessly.')
+  })
+
+  it('Check CTA Get Started button if it works', () => {
+    cy.get('.button').click()
+    cy.url()
+      .should('contain', 'docs/getting-started/intro')
+    cy.contains('h1','Introduction')
+  })
+
+  it('Check Docs page is present', () => {
+    cy.contains("Docs").click()
+    cy.url()
+      .should('contain', 'docs/getting-started/intro')
+    cy.contains('h1','Introduction')
+  })
+  
+  it('Check Blog page is present', () => {
+    cy.contains("Blog").click()
+    cy.url()
+      .should('contain', '/blog')
+    cy.contains('Recent posts')
+  })
+  
+  it('Check API page is present', () => {
+    cy.contains("API").click()
+    cy.url()
+      .should('contain', '/api')
+    cy.contains('Getting Started')
+  })
+  
+  it('Check Demo link is correct and Demo page is present', () => {
+    cy.contains('Demo').invoke('removeAttr', 'target').click()
+    cy.url()
+      .should('include', 'stackblitz.com/edit/jest-preview')
+    cy.contains('Jest Preview')
   })
 })
