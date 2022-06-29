4be078887fbc8e162c024c91d8eefdd7ab620018
Sergii <s.kirianov@gmail.com>
2022-06-29 18:41:40 +0100

    [171] changing script

--- website/scripts/syncExampleDocs.js
+++ website/scripts/syncExampleDocs.js
@@ -9,14 +9,19 @@ fs.readdir(path.join(__dirname, rootExamples), (err, directories) => {
 
   onlyDirectories.forEach(async dir => {
     const readmePath = path.join(__dirname, `${rootExamples}/${dir}/README.md`);
+    const docsPath = path.join(__dirname, `${webDocs}/${dir}.md`);
 
-    await fs.copyFile(readmePath, path.join(__dirname, `${webDocs}/${dir}.md`), (err) => {
-      if (err) {
-        throw new Error(err);
-      }
+    await fs.readFile(readmePath, 'utf-8', (err, data) => {
+      const fileWithFixedLinks = data.replaceAll('../../README.md', 'README.md');
 
-      console.log(`File ${dir}/README.md saved to Web Docs folder`);
-    });
+      console.log(fileWithFixedLinks);
+
+      fs.writeFile(docsPath, fileWithFixedLinks, 'utf-8', (err, data) => {
+        if (err) {
+          throw err
+        }
+      })
+    })
   });
 });
 
