de64544bf6521303c4a5271b6c4120e4220d01c6
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-08-25 21:59:06 +0700

    test: Add tests for LESS

---
+++ src/__tests__/less/index.test.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import { processLess } from '../../transform';
+import less from 'less';
+describe('Less', () => {
+  it('should compile LESS successfully', () => {
+    const result = processLess(path.resolve(__dirname, './style.less'));
+    expect(result).toMatchInlineSnapshot(`
+      ".less-p {
+        color: blue;
+      }
+      @media (min-width: 400px) {
+        .less-p {
+          font-size: 2rem;
+        }
+      }
+      #header {
+        color: black;
+      }
+      #header .navigation {
+        font-size: 12px;
+      }
+      #header .logo {
+        width: 300px;
+      }
+
+      "
+    `);
+  });
+});
