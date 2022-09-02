882c619dc041cfd94a56080f5b9f22ce9bdaf579
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2022-09-02 15:18:57 -0400

    jest-preview works

--- examples/vue/jest.config.js
+++ examples/vue/jest.config.js
@@ -1,3 +1,30 @@
+process.env.VUE_CLI_BABEL_TARGET_NODE = true;
+process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true;
+
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
-}
+  preset: '@vue/cli-plugin-unit-jest',
+  testURL: 'http://localhost/',
+  testEnvironment: 'jest-environment-jsdom-global',
+  transformIgnorePatterns: ['/node_modules/(?!babel/runtime)'],
+  snapshotSerializers: ['jest-serializer-vue-tjw'],
+  setupFilesAfterEnv: ['<rootDir>/tests/unit/setup.js'],
+  coverageDirectory: '<rootDir>/tests/unit/coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!**/node_modules/**'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.test.js'
+  ],
+  moduleNameMapper: {
+    '^@@/(.*)$': '<rootDir>/tests/$1',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  // Used by jest-preview
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.css$': 'jest-preview/transforms/css',
+    '^(?!.*\\.(js|css|json|vue)$)': 'jest-preview/transforms/file'
+  }
+};
--- examples/vue/vue.config.js
+++ examples/vue/vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  lintOnSave: false,
+  transpileDependencies: true,
+  parallel: false
+});
