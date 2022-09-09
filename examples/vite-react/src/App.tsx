74379f2e4c1dcd1ef0ee4ba1b13474e30ffaf05d
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-09-09 23:06:44 +0700

    example: add global styled-components for vite-react

--- examples/vite-react/src/App.tsx
+++ examples/vite-react/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import '~animate.css/animate.css';
 
 import logo2 from './assets/images/logo.svg';
@@ -10,64 +10,74 @@ import './assets/css/App.css';
 import './assets/_scss/style.scss';
 import styles from './style.module.css';
 
+const GlobalStyle = createGlobalStyle`
+  .sc-global {
+    color: yellow
+  }
+`;
+
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/logo.svg" className="App-logo" alt="logo" />
-        <img src={logo2} className="logo2" alt="logo2" />
-        <p>Vite Example</p>
-        <StyledText>This text is styled by styled-components</StyledText>
-        <p className={styles.green}>This text is styled by CSS Modules</p>
-        <p className="global-configured-sass">
-          This text is styled by global configured SASS
-        </p>
-        <p className="imported-sass">This text is styled by imported SASS</p>
-        <p className="load-path-sass">
-          This text is styled by SASS from load paths
-        </p>
-        <p className="text-xanh">TailwindCSS - Green</p>
-        <p className="animate__animated animate__bounce">
-          An animated element style using @use ~
-        </p>
-        <div className="animated fadeIn">
-          <p>An animated element style using import ~</p>
-          <p>Watch me fade in!</p>
-        </div>
-        <p>
-          <button
-            data-testid="increase"
-            type="button"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: <div data-testid="count">{count}</div>
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <header className="App-header">
+          <img src="/logo.svg" className="App-logo" alt="logo" />
+          <img src={logo2} className="logo2" alt="logo2" />
+          <p>Vite Example</p>
+          <StyledText>This text is styled by styled-components</StyledText>
+          <p className="sc-global">Global Style Components (Yellow)</p>
+          <p className={styles.green}>This text is styled by CSS Modules</p>
+          <p className="global-configured-sass">
+            This text is styled by global configured SASS
+          </p>
+          <p className="imported-sass">This text is styled by imported SASS</p>
+          <p className="load-path-sass">
+            This text is styled by SASS from load paths
+          </p>
+          <p className="text-xanh">TailwindCSS - Green</p>
+          <p className="animate__animated animate__bounce">
+            An animated element style using @use ~
+          </p>
+          <div className="animated fadeIn">
+            <p>An animated element style using import ~</p>
+            <p>Watch me fade in!</p>
+          </div>
+          <p>
+            <button
+              data-testid="increase"
+              type="button"
+              onClick={() => setCount((count) => count + 1)}
+            >
+              count is: <div data-testid="count">{count}</div>
+            </button>
+          </p>
+          <p>
+            Edit <code>App.tsx</code> and save to test HMR updates.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+        </header>
+      </div>
+    </>
   );
 }
 
