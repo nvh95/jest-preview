c64240c4801844663d84fc637a4d71bf0067d631
Justin Sun <i@jsun969.cn>
2022-12-30 13:59:23 +0800

    docs(website): initialize i18n

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -7,6 +7,7 @@ import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Sponsors from '@site/src/components/Sponsors';
 import Carbon from '../components/Carbon';
+import { translate } from '@docusaurus/Translate';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -14,13 +15,21 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">
+          {translate({
+            id: 'home.header.subtitle',
+            message: 'Debug your Jest tests. Effortlessly. 🛠🖼',
+          })}
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/getting-started/intro"
           >
-            Get Started
+            {translate({
+              id: 'home.header.get-started',
+              message: 'Get Started',
+            })}
           </Link>
           <a
             href="https://stackblitz.com/edit/jest-preview?file=src%2FApp.test.tsx,README.md"
@@ -28,7 +37,10 @@ function HomepageHeader() {
             rel="noopener noreferrer"
             className="button button--info button--lg"
           >
-            Try now
+            {translate({
+              id: 'home.header.try-now',
+              message: 'Try now',
+            })}
           </a>
         </div>
       </div>
